53b6a45de911e822ad4a466dd3df94a0b5a7c53f
omid <81848730+omid3699@users.noreply.github.com>
2026-02-14 13:35:41 +0430

    removed unused scripts dir

--- scripts/commit_last_work.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-# Script to stage and commit the recent changes made by the assistant.
-# Run from the project root: ./scripts/commit_last_work.fish
-
-# Stage the files we edited/created
-git add apps/users/models.py apps/users/admin.py apps/users/migrations/0003_remove_user_usermae.py
-
-# Commit with a short message
-git commit -m "Fix admin: show username (remove misspelled 'usermae')"
